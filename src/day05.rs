4403f1de2dac4cbbc1847f0155aa07eee5adb9ea
sigfalt <1689082+sigfalt@users.noreply.github.com>
2025-12-05 02:36:43 -0600

    day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,6 +1,9 @@
 #![allow(unused_imports)]
 
+use std::cmp::Ordering;
+use std::collections::BTreeMap;
 use std::ops::RangeInclusive;
+use ahash::AHashMap;
 use anyhow::*;
 use itertools::Itertools;
 use nom::character::complete::{char, line_ending, u64};
@@ -51,9 +54,55 @@ pub fn part1(input: &str) -> Result<u64> {
 	Ok(fresh_ingredients as u64)
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum FreshnessChange {
+	Start,
+	End
+}
+
 pub fn part2(input: &str) -> Result<u64> {
-	let _ = input;
-	Ok(0)
+	let (freshness_ranges, _) = parse(input);
+
+	let mut consolidated_fresh_ranges = BTreeMap::new();
+	freshness_ranges.iter().flat_map(|fresh_range| vec![
+		(fresh_range.start(), FreshnessChange::Start),
+		(fresh_range.end(), FreshnessChange::End)
+	]).for_each(|(ingredient_id, freshness)| {
+		consolidated_fresh_ranges.entry(ingredient_id)
+			.or_insert(vec![])
+			.push(freshness);
+	});
+
+	let mut fresh_ingredient_id_count = 0;
+	let mut active_freshness_ranges = 0;
+	let mut freshness_start_id = None;
+	consolidated_fresh_ranges.into_iter().for_each(|(ingredient_id, freshness_changes)| {
+		let was_fresh = active_freshness_ranges > 0;
+		active_freshness_ranges += freshness_changes.iter().map(|freshness| {
+			match freshness {
+				FreshnessChange::Start => 1,
+				FreshnessChange::End => -1,
+			}
+		}).sum::<i32>();
+		if was_fresh {
+			if active_freshness_ranges == 0 {
+				assert_ne!(freshness_start_id, None);
+				fresh_ingredient_id_count += ingredient_id - freshness_start_id.unwrap() + 1;
+				freshness_start_id = None;
+			}
+		} else {
+			if active_freshness_ranges > 0 {
+				assert_eq!(freshness_start_id, None);
+				freshness_start_id = Some(ingredient_id);
+			} else {
+				// wasn't fresh before but still isn't fresh now
+				// range of 1
+				fresh_ingredient_id_count += 1;
+			}
+		}
+	});
+
+	Ok(fresh_ingredient_id_count)
 }
 
 #[cfg(test)]
@@ -80,7 +129,7 @@ mod tests {
 
 	#[test]
 	fn test_part_two() -> Result<()> {
-		assert_eq!(0, part2(TEST)?);
+		assert_eq!(14, part2(TEST)?);
 		Ok(())
 	}
 }
